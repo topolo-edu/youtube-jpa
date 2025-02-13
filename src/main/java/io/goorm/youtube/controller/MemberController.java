3bf04f886f8987fdd26c1f0802ccd3f2fe1ca6b4
topolo.edu <topolo.edu@gmail.com>
2025-02-14 01:20:40 +0900

    import 정리

--- src/main/java/io/goorm/youtube/controller/MemberController.java
+++ src/main/java/io/goorm/youtube/controller/MemberController.java
@@ -9,7 +9,6 @@ import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
